1069c76b189b3f78ef2749f4bc2fa856ae8c0e1a
o.enomfonCSC101 <enomfon20@gmail.com>
2023-11-18 13:38:10 +0100

    CAT

---
+++ CAT/test-1/target/debug/deps/test_1.d
@@ -0,0 +1,5 @@
+C:\Users\USER\o.enomfonCSC-101\CAT\test-1\target\debug\deps\test_1.exe: src\main.rs
+
+C:\Users\USER\o.enomfonCSC-101\CAT\test-1\target\debug\deps\test_1.d: src\main.rs
+
+src\main.rs:
---
+++ CAT/test-1/target/debug/test-1.d
@@ -0,0 +1,1 @@
+C:\Users\USER\o.enomfonCSC-101\CAT\test-1\target\debug\test-1.exe: C:\Users\USER\o.enomfonCSC-101\CAT\test-1\src\main.rs
