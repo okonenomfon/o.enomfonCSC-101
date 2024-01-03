38e276f0891b2d4ed9290d65089f846ea6ea3bf9
o.enomfonCSC101 <enomfon20@gmail.com>
2024-01-03 17:38:43 +0100

    CA

--- CAT/test-2/src/main.rs
+++ CAT/test-2/src/main.rs
@@ -12,9 +12,9 @@ impl Company {
     fn lev_percent(&self) -> (f64, f64, f64) {
         let leverage = (self.shares - self.liabilities) / self.shares;
         let percentage_leverage = leverage * 100.0;
-        let leverage_multiplier = percentage_leverage * 2.0;
+        let multiple_lev = percentage_leverage * leverage;
 
-        (leverage, percentage_leverage, leverage_multiplier)
+        (leverage, percentage_leverage, multiple_lev)
     }
 }
 
@@ -39,7 +39,7 @@ fn login(company_name: &str) -> bool {
     name && pass
 }
 
-fn bonus(percentage_leverage: f64, liabilities: f64) -> f64 {
+fn five_percent(percentage_leverage: f64, liabilities: f64) -> f64 {
     if liabilities < 10_000_000.0 {
         percentage_leverage * 0.05
     } 
@@ -116,18 +116,30 @@ fn main() {
         .as_bytes())
         .expect("Error writing to file");
 
-        file.write_all(format!("| {:<28} | {:<12} | {:<10} | {:<10} | {:<8} | {:<10} | {:<10} | {:<7} |\n", "Name", "Year Founded", "Shares", "Liabilities", "Leverage", "% Leverage", "P. L. M", "Bonus")
+        file.write_all(format!("| {:<28} | {:<12} | {:<10} | {:<10} | {:<8} | {:<10} | {:<9} | {:<7} |\n", "Name", "Year Founded", "Shares", "Liabilities", "Leverage", "% Leverage", "P. L. M", "5% of %Lev")
         .as_bytes())
         .expect("Unable to write to file");
 
         for company in &companies {
             if company.name == company_name {
-                let (leverage, percentage_leverage, leverage_multiplier) = company.lev_percent();
-                let b = bonus(percentage_leverage, company.liabilities);
+                let (leverage, percentage_leverage, multiple_lev) = company.lev_percent();
+                let fp = five_percent(percentage_leverage, company.liabilities);
 
-                file.write_all(format!("| {:<28} | {:<12} | {:<10} | {:<11} | {:.6} | {:.7} | {:.6} | {:.5} |\n", company.name, company.year, company.shares, company.liabilities, leverage, percentage_leverage, leverage_multiplier, b)
+                file.write_all(format!("| {:<28} | {:<12} | {:<10} | {:<11} | {:.6} | {:.7} | {:.6} | {:.8} |\n", company.name, company.year, company.shares, company.liabilities, leverage, percentage_leverage, multiple_lev, fp)
                 .as_bytes())
                 .expect("Unable to write to file");
+
+                if company.shares > 20_000_000.0{
+                    let mut multi = File::create("multiple.txt").expect("Unable to create file");
+
+                    multi.write_all(format!("{:<50}\n", "MULTIPLE OF PERCENTAGE LEVERAGE  WITH SHARES GREATER THAN 20,000,000.00\n")
+                    .as_bytes())
+                    .expect("Error writing to file");
+
+                    multi.write_all(format!("{:<28} - {:.6}\n", company.name, multiple_lev)
+                    .as_bytes())
+                    .expect("Error writing to file");
+                }
 
                 println!("Data has been pushed to the .txt file.");
                 return;
@@ -135,7 +147,8 @@ fn main() {
         }
 
         println!("Oops! The company you entered is not on the list.");
-    } else {
+    } 
+    else {
         println!("No cheats... You have no access.");
     }
 }
